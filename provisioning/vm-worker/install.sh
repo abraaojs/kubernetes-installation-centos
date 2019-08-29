98603f57cb540679b0a986de88030c6d267372c6
Abraao Silva <abraaojs.dev@gmail.com>
2019-08-29 20:00:43 -0300

    add

--- provisioning/vm-worker/install.sh
+++ provisioning/vm-worker/install.sh
@@ -11,5 +11,7 @@ systemctl restart kubelet
 
 echo "[TASK 2] Join master"
 #Update the join token by executing the command on master node
+#echo kubeadm join $(kubeadm config view | grep ^controlPlaneEndpoint | awk '{print $2}') --token $(kubeadm token create) --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
+
 #kubeadm token create --print-join-command
 kubeadm join !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
